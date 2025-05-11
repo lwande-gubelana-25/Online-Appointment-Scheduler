670c7f0b3876178acc1dc7a4f3858505caea62fc
lwande-gubelana-25 <122478177+lwande-gubelana-25@users.noreply.github.com>
2025-05-11 11:20:43 +0200

    Create simple_factory.py

---
+++ Online-Appointment-Scheduler/tests/simple_factory.py
@@ -0,0 +1,11 @@
+from tests.factories.simple_factory import NotificationFactory
+
+def test_email_notification():
+    factory = NotificationFactory()
+    notification = factory.create_notification("email")
+    assert notification.send() == "Sending email notification"
+
+def test_sms_notification():
+    factory = NotificationFactory()
+    notification = factory.create_notification("sms")
+    assert notification.send() == "Sending SMS notification"
